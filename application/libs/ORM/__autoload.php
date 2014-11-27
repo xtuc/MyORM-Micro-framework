4acda2fc2edd7ea908b1c5bc43619a8d747770ed
Sven Sauleau <contact@xtuc.fr>
2014-11-27 15:52:52 +0100

    up

--- application/libs/ORM/__autoload.php
+++ application/libs/ORM/__autoload.php
@@ -60,7 +60,6 @@ class ORMAutoloader {
 		require libs . "ORM/Common.php";
 		require libs . "ORM/Exception/Exception.php";
 		require libs . "ORM/Exception/DALException.php";
-		require libs . "ORM/Debug/Debug.php";
 		require libs . "ORM/ORM.php";
 
 		if (version_compare(phpversion(), '5.3.0', '>=')) {
